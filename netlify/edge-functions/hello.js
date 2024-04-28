5b8880cbe6c425129fe54479bd7017717d22447e
francisco.ortega <francisco.ortega@softlond.co>
2024-04-28 12:59:53 -0500

    new-file

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config()
 export default async () => {
-  return  new Response(`Hola vengo desde la env: ${process.env.TEST}`);
+    console.log(process.env.TEST);
+  return  new Response(`Hola vengo desde la env:`);
 }
